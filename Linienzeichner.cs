b81e577b1ca2da6166550f2ef8aae98c5deb2e54
Fynn8962 <142886149+Fynn8962@users.noreply.github.com>
2024-08-30 17:08:31 +0200

    Add files via upload

---
+++ Linienzeichner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA_320_4207_Linienzeichner
+{
+    internal class Linienzeichner
+    {
+        private char _symbol = '*';
+
+
+        public Linienzeichner()
+        {
+
+        }
+        public Linienzeichner(char symbol)
+        {
+            
+            _symbol = symbol;
+        }
+
+        public string Zeichne(int anzahl)
+        {
+            return new string(_symbol, anzahl);
+        }
+
+
+        // 1. Überladung
+        public string Zeichne(int anzahl, char symbol)
+        {
+            return new string(symbol, anzahl);
+        }
+
+        // 2. Überladung
+        public string Zeichne(float prozent, int total)
+        {
+            return new string(_symbol, Convert.ToInt32(Math.Round(total * prozent)));
+
+        }
+
+        // 3. Überladung
+        public string Zeichne(float prozent, int total, char symbol)
+        {
+            return new string(symbol, Convert.ToInt32(Math.Round(total * prozent)));
+        }
+
+    }
+}
---
+++ program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA_320_4207_Linienzeichner
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Zeichen: ");
+             char input = Console.ReadLine().First();
+            Linienzeichner Zeichner = new Linienzeichner(input);
+            Console.WriteLine(Zeichner.Zeichne(10));
+
+
+            // 1. Überladung
+            Console.WriteLine(Zeichner.Zeichne(10, 'D'));
+
+            // 2. Überladung
+            Console.WriteLine(Zeichner.Zeichne(0.1f, 20));
+
+            // 3. Überladung
+            Console.WriteLine(Zeichner.Zeichne(0.2f, 40, '='));
+
+        }
+
+
+
+
+    }
+}
